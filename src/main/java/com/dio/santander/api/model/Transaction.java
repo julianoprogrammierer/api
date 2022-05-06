65a1e87cb0ed2c7dfb0fde1270b296b0888ffd32
julianoprogrammierer <julianoprogrammierer@gmail.com>
2022-05-06 17:37:38 +0000

    Create classes and mapping Entity

--- src/main/java/com/dio/santander/api/model/Transaction.java
+++ src/main/java/com/dio/santander/api/model/Transaction.java
@@ -2,11 +2,33 @@ package com.dio.santander.api.model;
 
 import java.time.LocalDateTime;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "transaction")
 public class Transaction {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
+
+    @Column(name = "date")
     private LocalDateTime dataHora;
+
+    @Column
     private String description;
+
+    @Column
     private Double value;
+
+    @Enumerated(EnumType.STRING)
     private transactionType type;
 
     public Integer getId() {
